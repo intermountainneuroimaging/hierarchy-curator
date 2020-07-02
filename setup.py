761baf3d383dacf6ea214a68d9dd57f42d4aac99
Nicolas Pannetier <nicolaspannetier@flywheel.io>
2020-07-02 14:21:29 -0700

    bounds setup install_requires to requirements.txt

--- setup.py
+++ setup.py
@@ -4,22 +4,16 @@ import os
 from setuptools import setup, find_packages
 
 NAME = "custom-curator"
-VERSION = os.getenv("0.1.0.dev")
+VERSION = os.getenv("CI_COMMIT_TAG", "0.1.0.dev")
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name=NAME,
     version=VERSION,
     description="Custom Curation Gear",
     project_urls={"Source": "https://github.com/flywheel-apps/custom-curator"},
-    install_requires=[
-        "flywheel-gear-toolkit==0.1.0rc4",
-        "lxml~=4.5",
-        "nibabel~=3.1",
-        "pandas~=1.0",
-        "Pillow~=7.1",
-        "piexif~=1.1",
-        "pydicom~=2.0",
-        "pypng~=0.0.20",
-    ],
+    install_requires=install_requires,
     packages=find_packages(),
 )
