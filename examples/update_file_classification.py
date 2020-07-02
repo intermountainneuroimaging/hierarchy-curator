c86f5564f78724bdaa8dc6009fb9b083f529c1e2
Nicolas Pannetier <nicolaspannetier@flywheel.io>
2020-07-02 11:56:28 -0700

    more cleanup of example script

--- examples/update_file_classification.py
+++ examples/update_file_classification.py
@@ -16,19 +16,6 @@ from custom_curator.reporters import CuratorErrorReporter
 log = logging.getLogger("my_curator")
 log.setLevel("DEBUG")
 
-SESSION_LABEL_CORRECTION = {
-    "screening": "Screening",
-    "w04": "Week_04",
-    "w12": "Week_12",
-    "w16": "Week_16",
-    "w20": "Week_20",
-    "w24": "Week_24",
-    "w28": "Week_28",
-    "w36": "Week_36",
-    "w48": "Week_48",
-    "REV1": "Relapse_Evaluation_1",
-}
-
 
 class Curator(curator.Curator):
 
@@ -41,34 +28,15 @@ class Curator(curator.Curator):
         self.error_reporter = CuratorErrorReporter(
             output_dir=gear_context.output_dir, project_label=project.label
         )
-        log.debug("Curating project with %s", self.input_file_one)
-        if self.input_file_one:
-            with open(self.input_file_one, "r") as fp:
-                updates = json.load(fp)
-            log.debug("Updates are %s", updates)
-            project.update(updates)
 
     def curate_subject(self, subject: flywheel.Subject):
         pass
 
     def curate_session(self, session: flywheel.Session):
-        log.info("Curating session %s", session.id)
-        try:
-            new_label = SESSION_LABEL_CORRECTION.get(session.label)
-            if new_label:
-                session.update({"label": new_label})
-        except Exception as exc:
-            self.error_reporter.write_session_error(
-                err_list=[],
-                err_str=str(exc),
-                subject_label=session.subject.id,
-                subject_id=session.subject.id,
-                session_label=session.label,
-                session_id=session.id,
-            )
+        pass
 
     def curate_acquisition(self, acquisition: flywheel.Acquisition):
-        acquisition.update_info({"Harsha": 10})
+        pass
 
     def curate_analysis(self, analysis):
         pass
