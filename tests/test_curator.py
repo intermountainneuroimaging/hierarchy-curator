b344fe32e4e8077eb5aa0409149c58f0eedb10e2
Nate Richman <naterichman@flywheel.io>
2020-11-20 15:52:55 -0600

    MAIN: Remove unneeded tests, fix imports

--- tests/test_curator.py
+++ tests/test_curator.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from custom_curator.curate import main, get_curator
+from flywheel_hierarchy_curator.curate import main, get_curator, load_curator
 
 ASSETS_DIR = Path(__file__).parent / "assets"
 
@@ -26,3 +26,23 @@ def test_curate_main_with_a_dummy_curator(fw_project):
     assert subject.reload().label == "Curated"
     assert session.reload().label == "Curated"
     assert acquisition.reload().label == "Curated"
+
+
+def test_load_curator_returns_module():
+    # Path as pathlib.Path
+    sys.modules.pop("dummy_curator", None)
+    assert "dummy_curator" not in sys.modules.keys()
+    mod = load_curator(ASSETS_DIR / "dummy_curator.py")
+    assert mod.filename == str(ASSETS_DIR / "dummy_curator.py")
+    assert "dummy_curator" in sys.modules.keys()
+
+    # Path as string
+    sys.modules.pop("dummy_curator")
+    assert "dummy_curator" not in sys.modules.keys()
+    mod = load_curator(str(ASSETS_DIR / "dummy_curator.py"))
+    assert mod.filename == str(ASSETS_DIR / "dummy_curator.py")
+    assert "dummy_curator" in sys.modules.keys()
+
+    # Returns None if path is corrupted
+    mod = load_converter(str(ASSETS_DIR / "doesnotexist.py"))
+    assert mod is None
