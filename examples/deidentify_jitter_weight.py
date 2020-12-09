65844a13e5b0986f4cd5bf4f5cc35dc601fc8c21
Nate Richman <naterichman@flywheel.io>
2020-12-09 15:24:10 -0600

    MAIN: Cannot update zipfile, need to write a new one and reupload

--- examples/deidentify_jitter_weight.py
+++ examples/deidentify_jitter_weight.py
@@ -1,6 +1,9 @@
 import json
 import logging
+from pathlib import Path
+import os
 import random
+import tempfile
 import zipfile
 
 import flywheel
@@ -18,10 +21,11 @@ class Curator(curator.HierarchyCurator):
         super().__init__(**kwargs)
         self.reporter = None
         if self.write_report:
-            log.info('Initiating reporter')
+            log.info("Initiating reporter")
             self.reporter = AggregatedReporter(
-                output_path=(Path(self.context.output_dir) / 'out.csv')
+                output_path=(Path(self.context.output_dir) / "out.csv")
             )
+        self.temp_dir = Path(tempfile.mkdtemp())
 
     def curate_project(self, project: flywheel.Project):
         pass
@@ -34,8 +38,12 @@ class Curator(curator.HierarchyCurator):
 
     def curate_acquisition(self, acquisition: flywheel.Acquisition):
         self.reporter.append_log(
-            container_type='acquisition',
-            label=acquisition.label, msg=f'Curating {len(acquisition.files)} files'
+            container_type="acquisition",
+            label=acquisition.label,
+            msg=f"Curating {len(acquisition.files)} files",
+            resolved=True,
+            err="",
+            search_key=""
         )
 
     def curate_analysis(self, analysis):
@@ -43,19 +51,53 @@ class Curator(curator.HierarchyCurator):
 
     def curate_file(self, file_: flywheel.FileEntry):
         log.info(f"Curating file {file_.name}")
-        if zipfile.is_zipfile(file_):
-            zip = zipfile.ZipFile(zip_file_path)
-            for n in range(len(zip.namelist())):
-                dcm_path = zip.extract(zip.namelist()[n], "/tmp")
+        if file_.name.endswith(".zip"):
+            f_read_path = self.temp_dir / f"old_{file_.name}"
+            f_write_path = self.temp_dir / file_.name
+
+            f_read_extract_path = self.temp_dir / f"old_{file_.name.split('.zip')[0]}"
+            f_write_extract_path = self.temp_dir / file_.name.split(".zip")[0]
+
+            file_.download(str(f_read_path))
+            # try:
+            zip_read = zipfile.ZipFile(str(f_read_path))
+            zip_write = zipfile.ZipFile(str(f_write_path), 'w')
+
+            for n in range(len(zip_read.namelist())):
+                dcm_path = zip_read.extract(
+                    zip_read.namelist()[n], path=str(f_read_extract_path)
+                )
                 if os.path.isfile(dcm_path):
                     try:
                         dcm = pydicom.dcmread(dcm_path)
                         # Randomly adjust patient weight for deidentify (Adding "jitter")
-                        setattr(dcm,'PatientWeight', dcm.get('PatientWeight')+ random.randint(-10,10))
-                    except:
-                        self.reporter.append_log(
-                            err="Cannot set PatientWeight",
-                            container_type='file',
-                            label=file_.name
+                        setattr(
+                            dcm,
+                            "StudyDescription",
+                            f"My deescr {random.randint(-10,10)}",
                         )
-        log.warning("Not a zip")
+                        new_path = f_write_extract_path / os.path.basename(dcm_path)
+                        dcm.save_as(new_path)
+                        zip_write.write(new_path)
+                    except Exception as e:
+                        if self.reporter:
+                            self.reporter.append_log(
+                                err=str(e),
+                                container_type="file",
+                                label=file_.name,
+                                search_key="",
+                                resolved=False,
+                                container_id=file_.id,
+                                msg="",
+                            )
+                        else:
+                            log.error(e)
+            zip_read.close()
+            zip_write.close()
+            file_.parent.upload_file(f_write_path)
+            # except:
+            #    self.reporter.append_log(
+            #        err="Cannot open zipfile",
+            #        container_type='file',
+            #        label=file_.name
+            #    )
