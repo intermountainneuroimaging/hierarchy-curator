8d44ce83fdc13cd6e47e116b132f40935216ae7c
Nate Richman <naterichman@flywheel.io>
2020-12-30 08:48:50 -0600

    MAIN: Remove optional requirements and write_report options.
    Change verbose -> debug and allow gear toolkit to handle logging init

--- run.py
+++ run.py
@@ -1,34 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+"""Hierarchy Curator gear entrypoint."""
 import logging
 
 from flywheel_gear_toolkit import GearToolkitContext
-from flywheel_gear_toolkit.utils import install_requirements
 
 from flywheel_hierarchy_curator import curate, parser
 
 log = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     with GearToolkitContext() as gear_context:
-        gear_context.init_logging(
-            default_config_name=(
-                "debug" if gear_context.config.get("verbose") else "info"
-            )
-        )
-        parent, curator_path, input_files, optional_requirements = parser.parse_config(
-            gear_context
-        )
+        gear_context.init_logging()
+        parent, curator_path, input_files = parser.parse_config(gear_context)
 
-        if optional_requirements:
-            log.info(f"Installing requirements from {optional_requirements}")
-            install_requirements(optional_requirements)
-
-        log.info(f"Curating {parent.container_type} {parent.label or parent.code}")
+        log.info(
+            "Curating %s %s",
+            parent.container_type,
+            (parent.label or parent.code),
+        )
         curate.main(
-            gear_context,
-            parent,
-            curator_path,
-            write_report=gear_context.config.get("write_report"),
-            **input_files,
+            gear_context, parent, curator_path, **input_files,
         )
