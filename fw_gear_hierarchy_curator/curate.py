6dfff4b7b6f6566167698a16b528132f52341bb4
Nate Richman <naterichman@flywheel.io>
2021-06-10 13:49:31 -0500

    MAIN: Add second-pass multithread implementation

--- fw_gear_hierarchy_curator/curate.py
+++ fw_gear_hierarchy_curator/curate.py
@@ -1,18 +1,52 @@
 """Hierarchy curator main interface."""
 import argparse
 import logging
+import math
 import sys
 from pathlib import Path
 import multiprocessing
 
 import flywheel
 from flywheel_gear_toolkit import GearToolkitContext
-from flywheel_gear_toolkit.utils import datatypes, walker
-from flywheel_gear_toolkit.utils.curator import get_curator, HierarchyCurator
+from flywheel_gear_toolkit.utils import datatypes, walker, reporters, curator
 
 sys.path.insert(0, str(Path(__file__).parents[1]))
 
 log = logging.getLogger(__name__)
+
+def worker(curator, children):
+    if curator.config.depth_first:
+        for child in children:
+            w = walker.Walker(
+                child,
+                depth_first=curator.config.depth_first,
+                reload=curator.config.reload,
+                stop_level=curator.config.stop_level
+            )
+            for container in w.walk(callback=curator.config.callback) :
+                try:
+                    if curator.validate_container(container):
+                        curator.curate_container(container)
+                except Exception:  # pylint: disable=broad-except pragma: no cover
+                    log.error("Uncaught Exception", exc_info=True)
+
+    else:
+        first_elem = children.pop(0)
+        w = walker.Walker(
+            first_elem,
+            depth_first=curator.config.depth_first,
+            reload=curator.config.reload,
+            stop_level=curator.config.stop_level
+        )
+        if children:
+            w.extend(children)
+        for container in w.walk(callback=curator.config.callback) :
+            try:
+                if curator.validate_container(container):
+                    curator.curate_container(container)
+            except Exception:  # pylint: disable=broad-except pragma: no cover
+                log.error("Uncaught Exception", exc_info=True)
+                
 
 
 def main(
@@ -29,45 +63,63 @@ def main(
         curator_path (Path-like): A path to a curator module.
         kwargs (dict): Dictionary of attributes/value to set on curator.
     """
-    curator = get_curator(context, curator_path, **kwargs)
-    if curator.multi:
-        def run_curate(queue: multiprocessing.Queue) -> None:
-            container = queue.get()
-            if curator.validate_container(container):
-                curator.curate_container(container)
-
-        queue = multiprocessing.Queue()
-        num_cpus = multiprocessing.cpu_count()
-        processes = []
-        for _ in range(num_cpus):
-            proc  = multiprocessing.Process(target=run_curate, args=(queue,))
-            proc.start()
-            processes.append(proc)
-
-    # Curator specific configuration unpack
-    config = curator.config
-
-    project_walker = walker.Walker(
+    # Initialize curator
+    curator = curator.get_curator(context, curator_path, **kwargs)
+    # Initialize walker from root container
+    root_walker = walker.Walker(
         parent,
-        depth_first=config.depth_first,
-        reload=config.reload,
-        stop_level=config.stop_level
+        depth_first=curator.config.depth_first,
+        reload=curator.config.reload,
+        stop_level=curator.config.stop_level
     )
-    try:  # pragma: no cover
-        if curator.multi:
-            for container in project_walker.walk(callback=config.callback):
-                queue.put((curator, container))
-        else:
-            for container in project_walker.walk(callback=config.callback):
+    # Initialize reporter if in config
+    if curator.config.report:
+        curator.reporter = reporters.AggregatedReporter(
+            curator.config.path,
+            format=curator.config.format,
+            multi=curator.config.multi
+        )
+    if curator.config.multi:
+        run_multiproc(curator, root_walker)
+    else:
+        for container in root_walker.walk(callback=curator.config.callback) :
+            try:
                 if curator.validate_container(container):
                     curator.curate_container(container)
+            except Exception:  # pylint: disable=broad-except pragma: no cover
+                log.error("Uncaught Exception", exc_info=True)
 
-    except Exception:  # pylint: disable=broad-except pragma: no cover
-        log.error("Uncaught Exception", exc_info=True)
-    
-    if curator.multi:
-        for proc in processes:
-            proc.join()
+def run_multiproc(curator, root_walker):
+    # Main multiprocessing entrypoint
+    workers = curator.config.workers
+    queue = multiprocessing.Queue()
+    if curator.reporter:
+        # Logger process
+        r = curator.reporter
+        logger = multiprocessing.Process(
+            target=reporters.worker,
+            args=(queue, r.output_path, r.output_type, r.writer_fn)
+        )
+        logger.start()
+    # Start by curating first container, then init workers on children
+    root = root_walker.next()
+    children = list(root_walker.deque)
+    children_per_worker = math.ceil(children / workers)
+    # Worker processes
+    worker_ps = []
+    while len(children):
+        containers = children[:children_per_worker]
+        children = children[children_per_worker:]
+        proc = multiprocessing.Process(target=worker, args=(curator, containers))
+        proc.start()
+        worker_ps.append(proc)
+    for proc in worker_ps:
+        proc.start()
+    for proc in worker_ps:
+        proc.join()
+    if curator.reporter:
+        curator.reporter.write('END')
+
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -98,3 +150,4 @@ if __name__ == "__main__":  # pragma: no cover
         additional_input_two=args.additional_input_two,
         additional_input_three=args.additional_input_three,
     )
+
