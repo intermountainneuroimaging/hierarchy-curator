ccb0e5d92fa42b5e75a418a8aa0b28c02cd285e3
Nate Richman <naterichman@flywheel.io>
2021-06-11 13:02:04 -0500

    MAIN: Update container assignments for curator

--- fw_gear_hierarchy_curator/curate.py
+++ fw_gear_hierarchy_curator/curate.py
@@ -2,23 +2,25 @@
 import argparse
 import logging
 import math
+import multiprocessing
 import os
 import sys
 from pathlib import Path
-import multiprocessing
 
 import flywheel
 from flywheel_gear_toolkit import GearToolkitContext
-from flywheel_gear_toolkit.utils import datatypes, walker, reporters, curator as c
+from flywheel_gear_toolkit.utils import curator as c
+from flywheel_gear_toolkit.utils import datatypes, reporters, walker
 
 sys.path.insert(0, str(Path(__file__).parents[1]))
 
 log = logging.getLogger(__name__)
 
+
 def worker(curator, children):
     log.debug(
-        f"Initializing worker with {len(children)} tree nodes. "+
-        f"PPID: {os.getppid()}, PID: {os.getpid()}"
+        f"Initializing worker with {len(children)} tree nodes. "
+        + f"PPID: {os.getppid()}, PID: {os.getpid()}"
     )
     if curator.config.depth_first:
         for child in children:
@@ -26,9 +28,9 @@ def worker(curator, children):
                 child,
                 depth_first=curator.config.depth_first,
                 reload=curator.config.reload,
-                stop_level=curator.config.stop_level
+                stop_level=curator.config.stop_level,
             )
-            for container in w.walk(callback=curator.config.callback) :
+            for container in w.walk(callback=curator.config.callback):
                 try:
                     if curator.validate_container(container):
                         curator.curate_container(container)
@@ -41,26 +43,25 @@ def worker(curator, children):
             first_elem,
             depth_first=curator.config.depth_first,
             reload=curator.config.reload,
-            stop_level=curator.config.stop_level
+            stop_level=curator.config.stop_level,
         )
         if children:
             w.extend(children)
-        for container in w.walk(callback=curator.config.callback) :
+        for container in w.walk(callback=curator.config.callback):
             try:
                 if curator.validate_container(container):
                     curator.curate_container(container)
             except Exception:  # pylint: disable=broad-except pragma: no cover
                 log.error("Uncaught Exception", exc_info=True)
-    log.debug(f'Worker with pid {os.getpid()} completed')
+    log.debug(f"Worker with pid {os.getpid()} completed")
     return
-                
 
 
 def main(
     context: GearToolkitContext,
     parent: datatypes.Container,
     curator_path: datatypes.PathLike,
-    **kwargs
+    **kwargs,
 ):
     """Curates a flywheel project using a curator.
 
@@ -77,58 +78,55 @@ def main(
         parent,
         depth_first=curator.config.depth_first,
         reload=curator.config.reload,
-        stop_level=curator.config.stop_level
+        stop_level=curator.config.stop_level,
     )
     # Initialize reporter if in config
     if curator.config.report:
         curator.reporter = reporters.AggregatedReporter(
             curator.config.path,
             format=curator.config.format,
-            multi=curator.config.multi
+            multi=curator.config.multi,
         )
     if curator.config.multi:
         run_multiproc(curator, root_walker)
     else:
-        for container in root_walker.walk(callback=curator.config.callback) :
+        for container in root_walker.walk(callback=curator.config.callback):
             try:
                 if curator.validate_container(container):
                     curator.curate_container(container)
             except Exception:  # pylint: disable=broad-except pragma: no cover
                 log.error("Uncaught Exception", exc_info=True)
 
+
 def run_multiproc(curator, root_walker):
     # Main multiprocessing entrypoint
     workers = curator.config.workers
-    queue = multiprocessing.Queue()
     if curator.reporter:
         # Logger process
-        log.info('Initializing logging process')
-        r = curator.reporter
-        logger = multiprocessing.Process(
-            target=reporters.worker,
-            args=(queue, r.output_path, r.output_type, r.writer_fn)
-        )
-        logger.start()
+        log.info("Initializing logging process")
+        reporter = curator.reporter
+        reporter.start()
     # Start by curating first container, then init workers on children
     root = root_walker.next()
+    if curator.validate_container(root):
+        curator.curate_container(root)
     children = list(root_walker.deque)
-    children_per_worker = math.ceil(len(children) / workers)
+    assignments = {i: [] for i in range(workers)}
+    for i, child in enumerate(children):
+        assignments[i % workers].append(child)
+
     # Worker processes
     worker_ps = []
-    while len(children):
-        containers = children[:children_per_worker]
-        children = children[children_per_worker:]
+    for key in assignments.keys():
+        containers = assignments[key]
         proc = multiprocessing.Process(target=worker, args=(curator, containers))
         proc.start()
         worker_ps.append(proc)
     for proc in worker_ps:
         proc.join()
-        log.debug(f'Process {proc.name} finished')
+        log.debug(f"Process {proc.name} finished")
     if curator.reporter:
-        curator.reporter.write('END')
-    
-
-
+        curator.reporter.write("END")
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -159,4 +157,3 @@ if __name__ == "__main__":  # pragma: no cover
         additional_input_two=args.additional_input_two,
         additional_input_three=args.additional_input_three,
     )
-
